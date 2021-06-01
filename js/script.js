b41c6d4e10aca21fea9c00f5eb8bc539b6a122a0
Boffin-ux <fnamily@mail.ru>
2021-06-01 12:25:04 +0300

    Выполнил усложненное задание Урок №4

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const getStr = str => {
    if (typeof str !== 'string') {
       return 'в качестве аргумента передана не строка';
    } else if (str.length > 30) {
-      return str.trim().substring(0, 30) + '...';
+      return str.trim().substring(0, 29) + '...';
    } else {
       return str.trim();
    }
