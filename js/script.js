d630ade80ec021b38de78dc98f320942481a96b0
Boffin-ux <fnamily@mail.ru>
2021-06-07 12:16:36 +0300

    Выполнил усложненное задание Урок №9

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
 'use strict';
-const date = new Date();
-const div = document.createElement('div');
-document.body.append(div);
 
-const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'];
-week.forEach((item, index) => {
-   const p = document.createElement('p');
-   if (index + 1 === date.getDay()) {
-      p.innerText = item;
-      p.style.fontWeight = 'bold';
-      div.append(p);
-      console.log(`%c${item}`, 'font-weight: bold');
-   } else if (index > 4) {
-      p.innerText = item;
-      p.style.fontStyle = 'italic';
-      div.append(p);
-      console.log(`%c${item}`, 'font-style:italic');
+const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'],
+   fMonth = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+const div = document.createElement('div'),
+   addPOne = document.createElement('p'),
+   addPTwo = document.createElement('p');
+document.body.append(div);
+div.append(addPOne);
+div.append(addPTwo);
+addPOne.style.color = 'red';
+addPOne.style.fontWeight = 'bold';
+addPTwo.style.color = 'green';
+addPTwo.style.fontWeight = 'bold';
+
+// функция для добавления 0 перед значениями
+const addZero = (num) => {
+   if (num >= 0 && num <= 9) {
+      return '0' + num;
    } else {
-      p.innerText = item;
-      div.append(p);
-      console.log(item);
+      return num;
    }
-});
+};
+// функция для склонения слов
+const addDeclension = (value, arr) => {
+   if (value % 10 === 1 && value !== 11) {
+      return arr[0];
+   } else if (value % 10 > 1 && value % 10 < 5 && (value < 10 || value >= 20)) {
+      return arr[2];
+   } else {
+      return arr[1];
+   }
+};
+
+setInterval(() => {
+   const data = new Date(),
+      years = data.getFullYear(),
+      days = data.getDay(),
+      date = data.getDate(),
+      months = data.getMonth(),
+      hours = data.getHours(),
+      minutes = data.getMinutes(),
+      seconds = data.getSeconds();
+
+   addPOne.textContent = `a) Сегодня ${week[days - 1]}, ${date} ${fMonth[months]} ${years} года,
+      ${hours} ${addDeclension(hours, ['час', 'часов', 'часа'])} 
+      ${minutes} ${addDeclension(minutes, ['минута', 'минут', 'минуты'])} 
+      ${seconds} ${addDeclension(seconds, ['секунда', 'секунд', 'секунды'])}`;
+
+   addPTwo.textContent = `б) ${addZero(date)}.${addZero(months)}.${years}
+   - ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
+}, 1000);
